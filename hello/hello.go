e97d4bf4ff33257a48c3cac8d5ea93f9a84e29a2
basokant <basokanthan@gmail.com>
2025-11-18 13:55:39 -0500

    Add support for french greeting

--- hello/hello.go
+++ hello/hello.go
@@ -3,16 +3,26 @@ package hello
 import "fmt"
 
 const spanish = "Spanish"
+const french = "French"
 const englishHelloPrefix = "Hello, "
 const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
 
 func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
 
+	prefix := englishHelloPrefix
+
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
 	}
-	return englishHelloPrefix + name
+
+	return prefix + name
 }
 
 func main() {
--- hello/hello_test.go
+++ hello/hello_test.go
@@ -20,6 +20,12 @@ func TestHello(t *testing.T) {
 		want := "Hola, Elodie"
 		assertCorrectMessage(t, got, want)
 	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Margot", "French")
+		want := "Bonjour, Margot"
+		assertCorrectMessage(t, got, want)
+	})
 }
 
 // testing.TB is an interface that testing.T and testing.B implement
