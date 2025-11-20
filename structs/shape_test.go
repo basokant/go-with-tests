76e77a7a4bf4b947b5058252c5aeb5e75dd17041
basokant <basokanthan@gmail.com>
2025-11-19 22:52:32 -0500

    Implement Circle Area method

--- structs/shape_test.go
+++ structs/shape_test.go
@@ -13,11 +13,23 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	rectangle := Rectangle{10.0, 10.0}
-	got := Area(rectangle)
-	want := 100.0
+	t.Run("rectangle", func(t *testing.T) {
+		rectangle := Rectangle{10.0, 10.0}
+		got := rectangle.Area()
+		want := 100.0
 
-	if got != want {
-		t.Errorf("got %v want %v", got, want)
-	}
+		if got != want {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
+
+	t.Run("circle", func(t *testing.T) {
+		circle := Circle{10.0}
+		got := circle.Area()
+		want := 314.1592653589793
+
+		if got != want {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
 }
