abce6b3c7a9964596caaf5483d34b7b761dcfc32
basokant <basokanthan@gmail.com>
2025-11-22 14:46:04 -0500

    Implement map Search function

---
+++ maps/dictionary_test.go
@@ -0,0 +1,15 @@
+package maps
+
+import "testing"
+
+func TestSearch(t *testing.T) {
+	dictionary := map[string]string{"test": "this is just a test"}
+
+	key := "test"
+	got := Search(dictionary, key)
+	want := "this is just a test"
+
+	if got != want {
+		t.Errorf("got %q want %q given %q", got, want, key)
+	}
+}
