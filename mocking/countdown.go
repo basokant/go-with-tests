3e26bdf981e3155c9b57dd3c7efd2d88cfb42318
basokant <basokanthan@gmail.com>
2025-11-24 23:32:28 -0500

    Add countDown iterator function

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -3,15 +3,26 @@ package mocking
 import (
 	"fmt"
 	"io"
+	"iter"
 )
 
 const finalWord = "Go!"
 const countdownStart = 3
 
 func Countdown(out io.Writer, sleeper Sleeper) {
-	for i := countdownStart; i > 0; i-- {
+	for i := range countDown(countdownStart) {
 		fmt.Fprintln(out, i)
 		sleeper.Sleep()
 	}
 	fmt.Fprint(out, finalWord)
+}
+
+func countDown(from int) iter.Seq[int] {
+	return func(yield func(int) bool) {
+		for i := from; i > 0; i-- {
+			if !yield(i) {
+				return
+			}
+		}
+	}
 }
