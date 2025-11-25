0e3d0037be52c866333d7bed69d8b423a4809a53
basokant <basokanthan@gmail.com>
2025-11-25 13:57:00 -0500

    Add ErrTimeout to Racer and test

--- concurrency/racer.go
+++ concurrency/racer.go
@@ -1,6 +1,7 @@
 package concurrency
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 	"time"
@@ -12,6 +13,8 @@ func Racer(a, b string) (winner string, err error) {
 	return ConfigurableRacer(a, b, defaultTimeout)
 }
 
+var ErrTimeout = errors.New("timed out waiting for race")
+
 func ConfigurableRacer(a, b string, timeout time.Duration) (winner string, err error) {
 	select {
 	case <-ping(a):
@@ -19,7 +22,7 @@ func ConfigurableRacer(a, b string, timeout time.Duration) (winner string, err e
 	case <-ping(b):
 		return b, nil
 	case <-time.After(timeout):
-		return "", fmt.Errorf("timed out waiting for %s and %s", a, b)
+		return "", errors.Join(ErrTimeout, fmt.Errorf("race between %s and %s", a, b))
 	}
 }
 
