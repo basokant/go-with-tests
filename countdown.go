7f9b78e6d7b33babb36d916f7322cecdbde871ea
basokant <basokanthan@gmail.com>
2025-11-24 23:19:12 -0500

    Add ConfigurableSleeper

--- countdown.go
+++ countdown.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"os"
+	"time"
 
 	"github.com/basokant/go-with-tests/mocking"
 )
 
 func main() {
-	sleeper := &mocking.DefaultSleeper{}
+	sleeper := &mocking.ConfigurableSleeper{Duration: 1 * time.Second, SleepFn: time.Sleep}
 	mocking.Countdown(os.Stdout, sleeper)
 }
