7979244bed7a53d36efe6dd723f825735e9d3fd1
basokant <basokanthan@gmail.com>
2025-11-24 23:19:12 -0500

    Add ConfigurableSleeper

--- mocking/sleeper.go
+++ mocking/sleeper.go
@@ -36,3 +36,20 @@ func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
 
 const write = "write"
 const sleep = "sleep"
+
+type ConfigurableSleeper struct {
+	Duration time.Duration
+	SleepFn  func(time.Duration)
+}
+
+func (cs *ConfigurableSleeper) Sleep() {
+	cs.SleepFn(cs.Duration)
+}
+
+type SpyTime struct {
+	durationSlept time.Duration
+}
+
+func (s *SpyTime) SetDurationSlept(duration time.Duration) {
+	s.durationSlept = duration
+}
