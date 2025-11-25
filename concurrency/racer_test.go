8540bcb09c99d580060ad2c97bdd75ca8a9827d9
basokant <basokanthan@gmail.com>
2025-11-25 13:28:18 -0500

    Add HTTP Racer function

---
+++ concurrency/racer_test.go
@@ -0,0 +1,29 @@
+package concurrency
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+func TestRacer(t *testing.T) {
+	slowServer := makeDelayedServer(20 * time.Millisecond)
+	fastServer := makeDelayedServer(0 * time.Millisecond)
+	defer slowServer.Close()
+	defer fastServer.Close()
+
+	want := fastServer.URL
+	got := Racer(slowServer.URL, fastServer.URL)
+
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
+
+func makeDelayedServer(delay time.Duration) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(delay)
+		w.WriteHeader(http.StatusOK)
+	}))
+}
