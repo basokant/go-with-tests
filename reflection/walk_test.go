3401303be2ede65695f99f0e1760498a3f6b312e
Ben Asokanthan <70033855+basokant@users.noreply.github.com>
2025-11-27 12:14:12 -0500

    Revert "fixup! Support multiple fields for Walk struct"
    This reverts commit 78ebbd4bcf384985572b9646998ed4af4bc19dc7.

--- reflection/walk_test.go
+++ reflection/walk_test.go
@@ -32,7 +32,7 @@ func TestWalk(t *testing.T) {
 	for _, test := range cases {
 		t.Run(test.Name, func(t *testing.T) {
 			var got []string
-			Walk(test.Input, func(input string) {
+			walk(test.Input, func(input string) {
 				got = append(got, input)
 			})
 
@@ -41,5 +41,22 @@ func TestWalk(t *testing.T) {
 			}
 		})
 	}
+	expected := "Chris"
+	var got []string
 
+	x := struct {
+		Name string
+	}{expected}
+
+	walk(x, func(input string) {
+		got = append(got, input)
+	})
+
+	if len(got) != 1 {
+		t.Errorf("wrong number of function calls, got %d want %d", len(got), 1)
+	}
+
+	if got[0] != expected {
+		t.Errorf("got %q, want %q", got[0], expected)
+	}
 }
