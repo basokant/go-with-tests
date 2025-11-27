c1ed4393b0de67ce9b9a95725e834771d13fe7ba
Ben Asokanthan <70033855+basokant@users.noreply.github.com>
2025-11-27 12:14:12 -0500

    Revert "fixup! Support maps in walk function"
    This reverts commit 72325a404965ba651ec0349f78ffea8babfd8107.

--- reflection/walk.go
+++ reflection/walk.go
@@ -7,25 +7,26 @@ import (
 func Walk(x any, fn func(input string)) {
 	val := getValue(x)
 
-	walkValue := func(value reflect.Value) {
-		Walk(value.Interface(), fn)
-	}
+	numberOfValues := 0
+	var getField func(int) reflect.Value
 
 	switch val.Kind() {
 	case reflect.String:
 		fn(val.String())
 	case reflect.Struct:
-		for i := 0; i < val.NumField(); i++ {
-			walkValue(val.Field(i))
-		}
+		numberOfValues = val.NumField()
+		getField = val.Field
 	case reflect.Slice, reflect.Array:
-		for i := 0; i < val.Len(); i++ {
-			walkValue(val.Index(i))
-		}
+		numberOfValues = val.Len()
+		getField = val.Index
 	case reflect.Map:
 		for _, key := range val.MapKeys() {
-			walkValue(val.MapIndex(key))
+			Walk(val.MapIndex(key).Interface(), fn)
 		}
+	}
+
+	for i := 0; i < numberOfValues; i++ {
+		Walk(getField(i).Interface(), fn)
 	}
 }
 
