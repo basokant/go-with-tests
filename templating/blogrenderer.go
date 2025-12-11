7b31021c4acd66d758e7fe268c686035f7f7d4bf
basokant <basokanthan@gmail.com>
2025-12-11 13:16:14 -0500

    Add postRenderer struct so template is only parsed once

--- templating/blogrenderer.go
+++ templating/blogrenderer.go
@@ -13,13 +13,21 @@ var (
 	postTemplates embed.FS
 )
 
-func Render(w io.Writer, p blogposts.Post) error {
+type PostRenderer struct {
+	templ *template.Template
+}
+
+func NewPostRenderer() (*PostRenderer, error) {
 	templ, err := template.ParseFS(postTemplates, "templates/*.gohtml")
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	if err := templ.ExecuteTemplate(w, "blog.gohtml", p); err != nil {
+	return &PostRenderer{templ: templ}, nil
+}
+
+func (r *PostRenderer) Render(w io.Writer, p blogposts.Post) error {
+	if err := r.templ.ExecuteTemplate(w, "blog.gohtml", p); err != nil {
 		return err
 	}
 
