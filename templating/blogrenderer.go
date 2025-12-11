abe1061352e9b4f798db93252c5a653e2100fee5
basokant <basokanthan@gmail.com>
2025-12-11 14:07:22 -0500

    Add RenderIndex method to PostRenderer
    Add top and bottom template to RenderIndex

--- templating/blogrenderer.go
+++ templating/blogrenderer.go
@@ -4,6 +4,7 @@ import (
 	"embed"
 	"html/template"
 	"io"
+	"strings"
 
 	blogposts "github.com/basokant/go-with-tests/files"
 )
@@ -12,6 +13,25 @@ var (
 	//go:embed "templates/*"
 	postTemplates embed.FS
 )
+
+type PostViewModel struct {
+	Title, SanitisedTitle, Description, Body string
+	Tags                                     []string
+}
+
+func sanitisedTitle(title string) string {
+	return strings.ToLower(strings.ReplaceAll(title, " ", "-"))
+}
+
+func NewPostViewModel(p blogposts.Post) PostViewModel {
+	return PostViewModel{
+		Title:          p.Title,
+		SanitisedTitle: sanitisedTitle(p.Title),
+		Description:    p.Description,
+		Body:           p.Body,
+		Tags:           p.Tags,
+	}
+}
 
 type PostRenderer struct {
 	templ *template.Template
@@ -27,9 +47,14 @@ func NewPostRenderer() (*PostRenderer, error) {
 }
 
 func (r *PostRenderer) Render(w io.Writer, p blogposts.Post) error {
-	if err := r.templ.ExecuteTemplate(w, "blog.gohtml", p); err != nil {
-		return err
+	return r.templ.ExecuteTemplate(w, "blog.gohtml", p)
+}
+
+func (r *PostRenderer) RenderIndex(w io.Writer, posts []blogposts.Post) error {
+	postViewModels := make([]PostViewModel, 0, len(posts))
+	for _, post := range posts {
+		postViewModels = append(postViewModels, NewPostViewModel(post))
 	}
 
-	return nil
+	return r.templ.ExecuteTemplate(w, "index.gohtml", postViewModels)
 }
