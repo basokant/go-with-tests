da899fe5c84c0c0f924d9004d3e34a6a14e337ac
basokant <basokanthan@gmail.com>
2025-12-10 21:53:09 -0500

    Add approval test for blogrenderer Render function

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/basokant/go-with-tests
 
 go 1.25.4
 
-require github.com/google/go-cmp v0.7.0
+require (
+	github.com/approvals/go-approval-tests v1.6.2
+	github.com/google/go-cmp v0.7.0
+)
--- templating/blogrenderer_test.go
+++ templating/blogrenderer_test.go
@@ -6,6 +6,8 @@ import (
 
 	blogposts "github.com/basokant/go-with-tests/files"
 	blogrenderer "github.com/basokant/go-with-tests/templating"
+
+	"github.com/approvals/go-approval-tests"
 )
 
 func TestRender(t *testing.T) {
@@ -18,18 +20,11 @@ func TestRender(t *testing.T) {
 
 	t.Run("it converts a single post into HTML", func(t *testing.T) {
 		buf := bytes.Buffer{}
-		err := blogrenderer.Render(&buf, aPost)
 
-		if err != nil {
+		if err := blogrenderer.Render(&buf, aPost); err != nil {
 			t.Fatal(err)
 		}
 
-		got := buf.String()
-		want := `<h1>hello world</h1><p>This is a description</p>Tags: <ul><li>go</li><li>tdd</li></ul>
-`
-
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		approvals.VerifyString(t, buf.String())
 	})
 }
